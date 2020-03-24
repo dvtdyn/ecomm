edafefa02c10fe76af2b71aee269eb37c191324c
dvtdyn <dayan.davut@gmail.com>
2020-03-24 22:00:56 +0100

    Remove node_modules

---
+++ repositories/users.js
@@ -0,0 +1,11 @@
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename')
+    }
+
+    this.filename = filename
+  }
+}
+
+new UsersRepository()
