8d6a5c7c3a8c37c823b3feeb8afc011a7d75add4
dvtdyn <dayan.davut@gmail.com>
2020-03-25 01:28:45 +0100

    Design a custom database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const usersRepo = require('./repositories/users')
 
 const app = express()
 
@@ -18,7 +19,18 @@ app.get('/', (req, res) => {
   `)
 })
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body
+
+  const existingUser = await usersRepo.getOneBy({ email })
+  if (existingUser) {
+    return res.send('Email in use')
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match')
+  }
+
   console.log(req.body)
   res.send('Account created!!')
 })
