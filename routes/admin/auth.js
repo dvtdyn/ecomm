23a487e15aef6d8f0edafde5bc90c84f6d33408f
dvtdyn <dayan.davut@gmail.com>
2020-03-26 23:15:49 +0100

    Add product listings and requiring authentication

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -30,7 +30,7 @@ router.post(
 
     req.session.userId = user.id
 
-    res.send('Account created!!')
+    res.redirect('/admin/products')
   }
 )
 
@@ -55,7 +55,7 @@ router.post(
 
     req.session.userId = user.id
 
-    res.send('You are signed in')
+    res.redirect('/admin/products')
   }
 )
 
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const multer = require('multer')
 
-const { handleErrors } = require('./middlewares.js')
+const { handleErrors, requireAuth } = require('./middlewares.js')
 const productsRepo = require('../../repositories/products.js')
+const productsIndexTemplate = require('../../views/admin/products/index.js')
 const productsNewTemplate = require('../../views/admin/products/new.js')
 
 const { requireTitle, requirePrice } = require('./validators.js')
@@ -10,14 +11,19 @@ const { requireTitle, requirePrice } = require('./validators.js')
 const router = express.Router()
 const upload = multer({ storage: multer.memoryStorage() })
 
-router.get('/admin/products', (req, res) => {})
+router.get('/admin/products', requireAuth, async (req, res) => {
+  const products = await productsRepo.getAll()
+  res.send(productsIndexTemplate({ products }))
+  console.log(productsIndexTemplate({ products }))
+})
 
-router.get('/admin/products/new', (req, res) => {
+router.get('/admin/products/new', requireAuth, (req, res) => {
   res.send(productsNewTemplate({}))
 })
 
 router.post(
   '/admin/products/new',
+  requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
@@ -27,7 +33,7 @@ router.post(
 
     await productsRepo.create({ title, price, image })
 
-    res.send('submitted')
+    res.redirect('/admin/products')
   }
 )
 
